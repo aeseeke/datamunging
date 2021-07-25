48dbbc92d71ce8c991e7c75509366650b34d514e
Devon Chase <devonchase18@gmail.com>
2021-07-25 17:13:18 -0500

    remove unncessary files

--- DataMunging.RetrieveTeamGoalSpread/Program.cs
+++ DataMunging.RetrieveTeamGoalSpread/Program.cs
@@ -7,7 +7,7 @@ namespace DataMunging.ConsoleApp
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Welcome to the DataMunging Retrieve Team Goal Spread Application \n");
             var fileFormatter = new FormatFileStreamData();
             Console.WriteLine(fileFormatter.RetrieveSmallestSpread(@"..\netcoreapp3.1\Data\football.dat", 6, 8));
         }
--- DataMunging.RetrieveWeatherSpreadConsoleApp/Program.cs
+++ DataMunging.RetrieveWeatherSpreadConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace DataMunging.RetrieveWeatherSpreadConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the DataMunging Retrieve Weather Spread Application \n");
             var fileFormatter = new FormatFileStreamData();
             Console.WriteLine(fileFormatter.RetrieveSmallestSpread(@"..\netcoreapp3.1\Data\weather.dat", 1, 2));
         }
